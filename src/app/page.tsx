1fe7ebf09c7caedcf5c26df7f973c86119101556
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-05-29 18:59:55 -0300

    imagem na page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Header from "@/components/header/Header";
-import Image from "next/image";
+import Image from "next/image"; 
+import imghotel from '/public/assets/imagemhotel/hotel-presidente-4s (1).jpg'
 
 
 export default function Home() {
   return (
     <main>
-      <Header/>
+      <Header/>   
+      <div className="relative w-full h-[calc(100vh-80px)] mt-4 overflow-hidden"> 
+        <Image
+          src={imghotel} alt="ImgHotel" layout="responsive" objectFit="cover" className="w-full h-full"
+        />
+      </div>
     </main>
   );
 }
