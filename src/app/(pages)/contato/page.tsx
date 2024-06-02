183239bce35fd6772103ab6095e00201de468064
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-06-02 17:11:58 -0300

    Update page.tsx

--- src/app/(pages)/contato/page.tsx
+++ src/app/(pages)/contato/page.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import Header from "@/components/header/Header";
+import Maps from "@/components/maps/maps";  
+import Infcontato from "@/components/infcontato/infcontato";  
 
-
-
-
-export default function Contato(){
-    return(
-        <main>
-            <Header/>
-        </main>
-    )
+export default function Contato() {
+  return (
+    <main>
+      <Header /> 
+      <Infcontato />
+      <Maps />
+    </main>
+  );
 }
