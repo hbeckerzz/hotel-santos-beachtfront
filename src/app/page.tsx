0d91032eb570334e016049d6994ed394d3cc69a6
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-06-02 20:00:53 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "@/components/header/Header";
 import Image from "next/image";
 import imghotel from '/public/assets/imagemhotel/hotel-presidente-4s (1).jpg';
-import Quadrado from "@/components/quadradoinfo/quadrado";
+import Quadrado from "@/components/quadradoinfo/quadrado"; 
+import BarraIncentivo from "@/components/Barra-incentivo/BarraIncentivo";
 
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
-      <Header/>
+      <Header/> 
+      <BarraIncentivo/>
       <div className="relative w-full flex-grow mt-4">
         <Image
           src={imghotel}
