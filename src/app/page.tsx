e8b0c1c9584040bb60d88f45172bf2da64013888
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-06-02 17:11:17 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
 import Header from "@/components/header/Header";
-import Image from "next/image"; 
-import imghotel from '/public/assets/imagemhotel/hotel-presidente-4s (1).jpg'
-
+import Image from "next/image";
+import imghotel from '/public/assets/imagemhotel/hotel-presidente-4s (1).jpg';
+import Quadrado from "@/components/quadradoinfo/quadrado";
 
 export default function Home() {
   return (
-    <main>
-      <Header/>   
-      <div className="relative w-full h-[calc(100vh-80px)] mt-4 overflow-hidden"> 
+    <main className="flex flex-col min-h-screen">
+      <Header/>
+      <div className="relative w-full flex-grow mt-4">
         <Image
-          src={imghotel} alt="ImgHotel" layout="responsive" objectFit="cover" className="w-full h-full"
+          src={imghotel}
+          alt="ImgHotel"
+          layout="responsive"
+          objectFit="cover"
+          className="w-full h-[calc(100vh-80px)]"
         />
+        <Quadrado />
       </div>
     </main>
   );
