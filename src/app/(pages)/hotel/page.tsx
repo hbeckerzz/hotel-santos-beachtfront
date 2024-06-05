4abfe84eff3ab26e3da237e445ade37dccb2e0d7
Danilo Tolini <122316522+Danilotolini@users.noreply.github.com>
2024-06-04 22:14:27 -0300

    Update page.tsx

--- src/app/(pages)/hotel/page.tsx
+++ src/app/(pages)/hotel/page.tsx
@@ -1,12 +1,13 @@
 import Header from "@/components/header/Header";
-
+import HotelComponente from "@/components/Hotelcomponentes/Hotelcomponentes";
 
 
 
 export default function Hotel(){
     return(
         <main>
-            <Header/>
+            <Header/> 
+            <HotelComponente />
         </main>
     )
 }
